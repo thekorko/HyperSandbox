2a9254cd1dd0ea600e88842bcdddf8f835796219
Sebas Korko <68623797+thekorko@users.noreply.github.com>
2022-03-09 00:52:14 -0300

    Controls, move and draw

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import pygame
-
+#Window size
 width = 800
 height = 600
-
+#Create a window
 window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Client")
+#Set the windows title
+pygame.display.set_caption("Hyper Sandbox")
 
 clientNumber = 0
 
@@ -15,23 +16,41 @@ class Player():
     self.width = width
     self.height = height
     self.color = color
+    self.rect = (x,y,width,height)
 
+  def draw(self,win):
+      pygame.draw.rect(win, self.color, self.rect)
 
-def redrawWindow():
+  def move(self):
+      keys = pygame.key.get_pressed()
 
+      if keys[pygame.K_LEFT]:
+          self.x -= self.vel
+
+      if keys[pygame.K_RIGHT]:
+          self.x += self.vel
+
+      if keys[pygame.K_UP]:
+          self.y -= self.vel
+
+      if keys[pygame.K_DOWN]:
+          self.y += self.vel
+
+def redrawWindow(window,player):
+  player.draw(window)
   window.fill((255,255,255))
   pygame.display.update()
 
 
 def main():
   run = True
-
+  p = Player(50,50,100,100,(255,0,0))
   while run:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
         pygame.quit()
-
-    redrawWindow()
+    p.move()
+    redrawWindow(window,p)
 
 main()
