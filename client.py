c18a26c664c222de10dbc41b5514c01d55b5cdcf
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-03-09 00:14:10 -0300

    toma korko

--- client.py
+++ client.py
@@ -4,3 +4,33 @@ width = 800
 height = 600
 
 window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+class Player():
+  def __init__(self, x, y, width, height, color):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.color = color
+
+
+def redrawWindow():
+
+  window.fill((255,255,255))
+  pygame.display.update()
+
+
+def main():
+  run = True
+
+  while run:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+        pygame.quit()
+
+    redrawWindow()
+
